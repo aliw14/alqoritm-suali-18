a36716e1182f1dba51f551c37007336759b0a426
ali <ali@MacBook-Pro-Ali.local>
2023-01-04 02:50:30 +0400

    mesaj

---
+++ alqoritm suali 18/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace alqoritm_suali_18;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int count = 0;
+        int[] numbers = { 1, 2, 3, 4, -5, 6, 7, -8, 9 };
+        for (int i = 1; i < numbers.Length; i++)
+        {
+
+            if ((numbers[i] > 0 && numbers[i - 1] < 0) || (numbers[i] < 0 && numbers[i - 1] > 0)) 
+
+                    count++;
+
+            
+        }
+        Console.Write(count);
+
+
+
+    }
+}
+
